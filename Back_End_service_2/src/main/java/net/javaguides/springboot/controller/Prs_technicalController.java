4a1d477259856650141a59d86daa8cabeeb1e742
gishalinrufina <gishalin.rufina@gmail.com>
2023-06-25 21:29:15 +0530

    updated

--- Back_End_service_2/src/main/java/net/javaguides/springboot/controller/Prs_technicalController.java
+++ Back_End_service_2/src/main/java/net/javaguides/springboot/controller/Prs_technicalController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("prs_technical")
